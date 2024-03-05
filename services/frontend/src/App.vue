df6625a9ed67903798e26e41f42bf6bc7686a052
NANDIorg <super.egor.mamonov@yandex.ru>
2024-03-05 19:11:51 +0700

    new front

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -1,107 +1,26 @@
-
 <template>
-   <v-app>
-    <HeaderApp />
-      <v-main>
-        <RouterView />
-      </v-main>
-  </v-app>
-  
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-
 <script>
-import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import HeaderApp from '@/components/HeaderApp.vue'
-import { useUserStore } from '@/stores/user.js'
 
 export default {
-  setup() {
-    const userStore = useUserStore()
-
-    return {
-      userStore
-    }
-  },
-
+  name: 'App',
   components: {
-    HelloWorld,
-    RouterLink,
-    RouterView,
-    HeaderApp
-  },
-
-  async mounted() {
-    // console.log('this.userStore', this.userStore)
-    await this.userStore.checkUserLoggedIn();
-    console.log('this.userStore', this.userStore)
-    console.log('userLoggedIn', this.userStore.userLoggedIn)
+    HelloWorld
   }
 }
 </script>
 
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
