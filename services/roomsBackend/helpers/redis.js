9ed632177d871936895d9f8e1a57e0cae057a84c
mamonov.ep <nandi.job.off@gmail.com>
2024-08-12 01:18:13 +0700

    add wss rooms

---
+++ services/roomsBackend/helpers/redis.js
@@ -0,0 +1,31 @@
+import { createClient } from 'redis';
+import config from './config.tmpl.cjs';
+
+let client;
+
+async function init() {
+    client = createClient({
+        password: config.redisSecret
+    }).on('error', err => console.log('Redis Client Error', err))
+
+    await client.connect();
+
+    // await client.set('key', 'value');
+    // const value = await client.get('key');
+    // await client.disconnect();
+
+}
+
+await init();
+
+async function setCache(key, value) {
+    await client.set(key, JSON.stringify(value));
+}
+async function getCache(key) {
+    return JSON.parse(await client.get(key));
+}
+async function delCache(key) {
+    await client.del(key);
+}
+
+export { setCache, getCache, delCache }
