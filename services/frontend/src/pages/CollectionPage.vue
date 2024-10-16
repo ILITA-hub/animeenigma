65086832f001ccdb5dce2f412205e9ab7da0aa10
alnshrpv <alnshrpv666@mail.ru>
2024-10-17 00:56:06 +0800

    add stats-banner and some styles

--- services/frontend/src/pages/CollectionPage.vue
+++ services/frontend/src/pages/CollectionPage.vue
@@ -1,9 +1,9 @@
 <template>
   <div ref="container" class="container">
-
+    <a @click="handleBack" class="back"><span class="mdi mdi-arrow-left"></span> Назад</a>
     <div class="banner">
       <v-card class="main-banner">
-        <v-img class="bg-picture" height="211" :src="collection?.image || '/zoro.jpg'" cover></v-img>
+        <v-img class="bg-picture" height="260" :src="collection?.image || '/zoro.jpg'" cover></v-img>
         <div class="header">
           <div class="text-container">
             <v-card-title class="title">
@@ -11,14 +11,22 @@
             </v-card-title>
           </div>
         </div>
-        
       </v-card>
+    <v-card class="player-stats-banner">
+      <v-card-title class="stats-title">Статистика игрока</v-card-title>
+      <div class="stats-windows">
+        <div class="stat-window" v-for="(stat, index) in stats" :key="index">
+          <div class="stat-text">{{ stat }}</div>
+        </div>
+      </div>
+    </v-card>
     </div>
     <div class="content">
       <div class="sidebar">
         <FilterAnime :incoming-selected-genres="selectedGenres" :incoming-selected-years="selectedYears"
           @update:selectedGenres="setSelectedGenres" @update:selectedYears="setSelectedYears" />
         <div class="main-content">
+          <div class="result">Результаты поиска</div>
           <AnimeCard v-for="(video, index) in videos" :key="index" :anime="video" :isCollection="true" />
         </div>
       </div>
@@ -37,10 +45,10 @@ const {id} = defineProps({
   id: String,
 });
 
+const stats = ref(['Очков', 'Викторин создано', 'Викторин пройдено', 'Коллекций создано']);
 const collectionStore = useCollectionStore()
 const selectedGenres = ref([]);
 const selectedYears = ref([]);
-const searchQuery = ref('');
 const collection = computed(()=> collectionStore.collection)
 const videos = computed(()=> collection.value.videos)
 
@@ -62,10 +70,89 @@ function setSelectedGenres(params) {
 </script>
 
 <style scoped>
+
+.result {
+  color: white;
+  font-family: Montserrat;
+  font-size: 28px;
+  font-weight: 700;
+  line-height: 34.13px;
+  margin: 0;
+}
+
+.back {
+  color: white;
+  font-family: Montserrat;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 19.5px;
+  text-align: left;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  left: 29px;
+  top: 13px;
+}
+
+.back .mdi {
+  color: rgba(51, 169, 255, 1);
+  margin-right: 5px;
+}
+
+.player-stats-banner {
+  max-width: 350px;
+  padding: 0px 0px 0px 23px;
+  background-color: rgba(33, 35, 53, 1);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
+.stats-title {
+  font-family: Montserrat;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 19.5px;
+  text-align: left;
+  color: white;
+  padding-left: 0px;
+  padding-bottom: 15px;
+}
+
+.stats-windows {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+}
+
+.stat-window {
+  width: 144px;
+  height: 101px;
+  background-color: rgba(255, 255, 255, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  transition: background-color 0.3s;
+  text-align: center;
+}
+
+.stat-window:hover {
+  background-color: rgba(20, 112, 239, 1);
+}
+
+.stat-text {
+  font-family: Montserrat;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 14.63px;
+  color: white;
+}
+
 .container {
   display: flex;
   flex-direction: column;
-  gap: 20px;
   color: white;
 }
 
@@ -73,7 +160,8 @@ function setSelectedGenres(params) {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 20px;
+  padding: 15px;
+  gap: 10px;
 }
 
 .main-banner {
